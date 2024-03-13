048f24ac16e7355d35a99dc6062baa291e06c28d
Voushikha <149755814+Voushikha@users.noreply.github.com>
2024-03-13 12:58:48 +0800

    Add files via upload

---
+++ WikiApplication/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WikiApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.textBoxStructure = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.labelStructure = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(21, 28);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(150, 20);
+            this.textBoxName.TabIndex = 17;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(195, 72);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(61, 30);
+            this.buttonClear.TabIndex = 16;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(275, 30);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(61, 30);
+            this.buttonEdit.TabIndex = 15;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(275, 72);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(61, 30);
+            this.buttonDelete.TabIndex = 14;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(195, 30);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(61, 30);
+            this.ButtonAdd.TabIndex = 13;
+            this.ButtonAdd.Text = "Add";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 159;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Category";
+            this.columnHeader2.Width = 238;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(364, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(367, 412);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.labelCategory_Click);
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(21, 74);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(150, 20);
+            this.textBoxCategory.TabIndex = 18;
+            // 
+            // textBoxStructure
+            // 
+            this.textBoxStructure.Location = new System.Drawing.Point(21, 122);
+            this.textBoxStructure.Name = "textBoxStructure";
+            this.textBoxStructure.Size = new System.Drawing.Size(150, 20);
+            this.textBoxStructure.TabIndex = 19;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(21, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 21;
+            this.labelName.Text = "Name";
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Location = new System.Drawing.Point(21, 58);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(49, 13);
+            this.labelCategory.TabIndex = 22;
+            this.labelCategory.Text = "Category";
+            this.labelCategory.Click += new System.EventHandler(this.labelCategory_Click);
+            // 
+            // labelStructure
+            // 
+            this.labelStructure.AutoSize = true;
+            this.labelStructure.Location = new System.Drawing.Point(21, 106);
+            this.labelStructure.Name = "labelStructure";
+            this.labelStructure.Size = new System.Drawing.Size(50, 13);
+            this.labelStructure.TabIndex = 23;
+            this.labelStructure.Text = "Structure";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 536);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(751, 22);
+            this.statusStrip1.TabIndex = 25;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(590, 22);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(141, 30);
+            this.buttonSearch.TabIndex = 26;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(499, 479);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(103, 34);
+            this.buttonSave.TabIndex = 27;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(195, 116);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(61, 30);
+            this.buttonLoad.TabIndex = 28;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView2.Location = new System.Drawing.Point(21, 203);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(315, 310);
+            this.listView2.TabIndex = 29;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Definition";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(364, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 34);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 558);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.labelStructure);
+            this.Controls.Add(this.labelCategory);
+            this.Controls.Add(this.textBoxStructure);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.TextBox textBoxStructure;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelCategory;
+        private System.Windows.Forms.Label labelStructure;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WikiApplication/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WikiApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int ROW = 12;
+        static int COL = 4;
+        string[,] TwoDimAry = new string[ROW, COL];
+        private object columnHeader1Name;
+        private object textBoxstructure;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //   listView1.Items.Clear();
+            for (int i = 0; i < ROW; i++)
+            {
+                ListViewItem item = new ListViewItem(TwoDimAry[i, 0]);
+                item.SubItems.Add(TwoDimAry[i, 1]);
+                item.SubItems.Add(TwoDimAry[i, 2]);
+                listView1.Items.Add(item);
+                listView1.Sort();
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(textBoxName.Text) &&
+                !string.IsNullOrEmpty(textBoxCategory.Text))
+              //!string.IsNullOrEmpty(textBoxStructure.Text) &&
+             // !string.IsNullOrEmpty(textBoxDefinition.Text))
+            {
+                // Assuming listView1 is your ListView control
+                // Create an array to hold the values from the textboxes
+                string[] itemValues = new string[] {
+            textBoxName.Text,
+            textBoxCategory.Text,
+         // textBoxStructure.Text,
+          //textBoxDefinition.Text
+        };
+
+                // Create a ListViewItem with the array of values
+                ListViewItem item = new ListViewItem(itemValues);
+
+
+
+                // Add the ListViewItem to the ListView
+                listView1.Items.Add(item);
+
+                // items from textboxes will be clear after being displayed on listview
+                textBoxName.Clear();
+                textBoxCategory.Clear();
+            //  textBoxStructure.Clear();
+             // textBoxDefinition.Clear();
+            }
+
+        }
+
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            
+      // textBoxName.Text
+             
+
+
+            try
+            {
+                listView1.SelectedItems[0].SubItems[0].Text = textBoxName.Text;
+                listView1.SelectedItems[0].SubItems[1].Text = textBoxCategory.Text;
+          //    listView1.SelectedItems[0].SubItems[2].Text = textBoxStructure.Text;
+          //    listView1.SelectedItems[0].SubItems[3].Text = textBoxDefinition.Text;
+            }
+            catch { }
+
+       
+        }
+    
+
+    
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelCategory_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void labelCategory_Click(object sender, EventArgs e)
+        {
+     
+           
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // initial path set to nothing to ensure no reading or writing until a file is selected
+            string path = "";
+            // track changed status of textbox
+            bool textChanged = false;
+            // file loading flag
+            bool fileLoading = false;
+        }
+    }
+}
